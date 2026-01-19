d5cafe032334427d06447f8e748109483d744662
Turb0Pasca1 <pascalwernicke9@gmail.com>
2026-01-19 10:21:55 +0100

    test new acknowledge command

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from protocol import ProtocolDecoder
 
 with TCPConnection('192.168.1.106', 2000) as conn:
     decoder = ProtocolDecoder(conn)
-    decoded = decoder.get_SLS(500)
+    decoded = decoder.get_S1S()
+    print(decoded)
