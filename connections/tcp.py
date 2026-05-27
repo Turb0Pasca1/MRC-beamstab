23cd88ba169bdce0bdd7b502499374b3b4c8fb9a
Turb0Pasca1 <wernicke@mbi-berlin.de>
2026-05-27 11:19:39 +0200

    Adapted to serial.py docstrings

--- connections/tcp.py
+++ connections/tcp.py
@@ -3,13 +3,25 @@ import socket
 from .base import BaseConnection
 
 class TCPConnection(BaseConnection):
-    def __init__(self, host: str, port: int, timeout=2):
+    def __init__(self, host: str, port: int = 2000, timeout: float = 2.0):
+        """
+        Initializes the TCP/IP connection for the MRC beam stabilization system.
+        
+        Port 2000 is default for ETH-based systems.
+        
+        :param host: IP-address.
+        :param port: The port.
+        :param timeout: Read timeout in seconds.
+        """
         self.host = host
         self.port = port
         self.timeout = timeout
         self.sock = None
 
     def open(self):
+        """
+        Opens the TCP/IP connection.
+        """
         # socket.create_connection() is a wrapper of socket.connect() 
         self.sock = socket.create_connection(
             (self.host, self.port),
@@ -17,12 +29,24 @@ class TCPConnection(BaseConnection):
         )
 
     def close(self):
+        """
+        Closes the TCP/IP connection.
+        """
         if self.sock:
             self.sock.close()
             self.sock = None
 
     def write(self, data: bytes):
-        self.sock.sendall(data)
+        """
+        Sends uppercase ASCII command names and binary-coded parameters[cite: 9, 11].
+        """
+        if self.sock:
+            self.sock.sendall(data)
 
     def read(self, size: int) -> bytes:
-        return self.sock.recv(size)
+        """
+        Reads binary-coded return values[cite: 12].
+        """
+        if self.sock:
+            return self.sock.recv(size)
+        return b""
